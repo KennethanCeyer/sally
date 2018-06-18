7ddf545d2dba605960a4b6b5e0d54a9142d1b0a5
Han SungMin <kenneth.ceyer@navercorp.com>
2018-06-18 21:59:27 +0900

    WIP: add more command(unstable)

--- sally/__main__.py
+++ sally/__main__.py
@@ -1,6 +1,17 @@
-from sally import dataset
+import argparse
+
 from sally.dataset import DataSet
 
+__version__ = '0.1'
+
 if __name__ == '__main__':
-    dataset = DataSet()
-    dataset.download()
+    parser = argparse.ArgumentParser(description='ML dataset management tool')
+    parser.add_argument('--version', action='version', version=('%%(prog)s %s' % __version__), help='get version')
+    parser.add_argument('install', default=False, type=bool, help='install ML dataset')
+    args = parser.parse_args()
+
+    if args.install:
+        dataset = DataSet()
+        dataset.download()
+    else:
+        parser.print_help()
